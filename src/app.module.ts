b9500ee2779f0e227a6c9ab6a92bb9b4ca05d4ef
Manuel <manueljuliocasanova@gmail.com>
2023-08-02 01:46:09 -0400

    first commit t-m-mjcr

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,52 @@
+// import { Module } from '@nestjs/common';
+// import { TasksModule } from './tasks/tasks.module';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { AuthModule } from './auth/auth.module';
+// import { ConfigModule, ConfigService } from '@nestjs/config';
+// import { configValidationSchema } from './config.schema';
+
+// @Module({
+//   imports: [
+//     ConfigModule.forRoot({
+//       envFilePath: [`.env.stage.${process.env.STAGE}`],
+//       validationSchema: configValidationSchema,
+//     }),
+//     TasksModule,
+//     TypeOrmModule.forRootAsync({
+//       imports: [ConfigModule],
+//       inject: [ConfigService],
+//       useFactory: async (configService: ConfigService) => {
+//         const isProduction = configService.get(`STAGE`) === 'prod';
+//         return {
+//           ssl: isProduction,
+//           extra: {
+//             ssl: isProduction ? { rejectUnauthorized: false } : null,
+//           },
+//           type: 'postgres',
+//           autoLoadEntities: true,
+//           synchronize: true,
+//           host: configService.get('DB_HOST'),
+//           port: configService.get('DB_PORT'),
+//           username: configService.get('DB_USERNAME'),
+//           password: configService.get('DB_PASSWORD'),
+//           database: configService.get('DB_DATABASE'),
+//         };
+//       },
+//     }),
+//     AuthModule,
+//   ],
+//   controllers: [],
+//   providers: [],
+// })
+// export class AppModule {}
+
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { configValidationSchema } from './config.schema';
+import * as fs from 'fs';
 
 @Module({
   imports: [
@@ -16,12 +59,20 @@ import { configValidationSchema } from './config.schema';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => {
-        const isProduction = configService.get(`STAGE`) === 'prod';
+        // const isProduction = configService.get(`STAGE`) === 'prod';
         return {
-          ssl: isProduction,
-          extra: {
-            ssl: isProduction ? { rejectedUnauthorized: false } : null,
-          },
+          ssl: false,
+          // ssl: isProduction, // Set this to true to enable SSL in production
+          // extra: {
+          //   ssl: isProduction
+          //     ? {
+          //         rejectUnauthorized: false,
+          //         ca: fs.readFileSync('path/to/ca.pem'), // Add the correct path to your CA certificate
+          //         cert: fs.readFileSync('path/to/client-cert.pem'), // Add the correct path to your client certificate
+          //         key: fs.readFileSync('path/to/client-key.pem'), // Add the correct path to your client private key
+          //       }
+          //     : null,
+          // },
           type: 'postgres',
           autoLoadEntities: true,
           synchronize: true,
