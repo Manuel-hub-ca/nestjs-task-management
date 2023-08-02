c5522247e0dd8045097895acd9eb747db04942f5
Manuel <manueljuliocasanova@gmail.com>
2023-08-02 04:54:51 -0400

    dsds

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
 
-@Controller('')
+@Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
